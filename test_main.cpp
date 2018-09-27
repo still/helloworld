3a65d0626c3dfeacec65a13d9b9840f806aeee5d
still <stillarty@ya.ru>
2018-09-27 11:50:33 +0300

    Original test returned.

--- test_main.cpp
+++ test_main.cpp
@@ -8,8 +8,7 @@ BOOST_AUTO_TEST_SUITE(test_suite_main)
 
 BOOST_AUTO_TEST_CASE(test_version_valid)
 {
-    BOOST_CHECK(false);
-//    BOOST_CHECK( version() > 0 );
+    BOOST_CHECK( version() > 0 );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
