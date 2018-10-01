89583c4c92f7c1c5ede92e50ef24be22b942a390
still <stillarty@ya.ru>
2018-10-01 10:53:49 +0300

    Fixed output.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@ using namespace std;
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("Hello World version {}", version());
+//  оставил использование spdlog "на память"),
+//  реализовал вывод, требуемый в ДЗ из pdf-ки
+//    auto logger = spdlog::stdout_logger_mt("console");
+//    logger->info("Hello World version {}", version());
+    cout << "build " << version() << endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
